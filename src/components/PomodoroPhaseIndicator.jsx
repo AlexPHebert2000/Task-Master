610f34c7a41a3439b3a1abc73299f7b50bfc0a1e
Alex <alexphebert@gmail.com>
2025-10-15 15:17:14 -0500

    ADD ability to cycle through phases

--- src/components/PomodoroPhaseIndicator.jsx
+++ src/components/PomodoroPhaseIndicator.jsx
@@ -1,6 +1,6 @@
 export default ({title, active}) => {
   return (
   <div>
-    <div className={`${active ? 'bg-green-700' : 'bg-amber-300'}`}>{title}</div>
+    <div className={`${active ? 'bg-gray-300 drop-shadow-black/50 drop-shadow-md' : 'bg-gray-600 drop-shadow-[0px, -6px, 4px]'}  px-2 rounded-lg`}>{title}</div>
   </div>
 )}
--- src/views/Pomodoro.jsx
+++ src/views/Pomodoro.jsx
@@ -30,7 +30,7 @@ export default () => {
   },[running]);
 
   
-  const phases = [
+  const phaseTemplate = [
     {title: "Work", active: true},
     {title: "Short Break", active: false},
     {title: "Work", active: false},
@@ -42,21 +42,25 @@ export default () => {
   ]
 
   const phaseReducer = (state, {type, payload}) => {
-    const acceptedActions = ["next"];
+    const acceptedActions = ["set"];
     if (! acceptedActions.includes(type)){ return state }
 
     switch(type){
-      case("next"):
-        const newPhases = [...state];
-        newPhases[payload].active = false;
-        newPhases[(payload + 1) % 7] = true;
-        return newPhases;
-      default:
-        return state;
+      case("set"):
+        if (payload < 0){ return state }
+        else if (payload > 7){ payload = 0}
+        const newPhaseArr = [...state.phases]
+        newPhaseArr[payload].active = true;
+        newPhaseArr[state.currentIndex].active = false;
+        return {
+          ...state,
+          currentIndex: payload,
+          phases: newPhaseArr
+        }
     }
   }
 
-  const [activePhases, activePhaseDispatch] = useReducer(phaseReducer, phases)
+  const [{phases, currentIndex}, activePhaseDispatch] = useReducer(phaseReducer, {currentIndex: 0, phases: phaseTemplate})
   return (
     <div>
       <h1>
@@ -67,11 +71,12 @@ export default () => {
           "You broke the site, thanks" 
         }
       </h1>
-      <div>
-        {activePhases.map(({title, active}) => 
-          <PomodoroPhaseIndicator title={title} active={active}/>
+      <div className="flex bg-gray-500 p-2 w-2xl justify-between rounded-lg">
+        {phases.map(({title, active}, index) => 
+          <PomodoroPhaseIndicator title={title} active={active} index={index}/>
         )}
       </div>
+      <button onClick={() => {activePhaseDispatch({type: "set", payload: currentIndex + 1})}}>Next</button>
       <h1>{`${Math.floor(time / 60)}:${time % 60 < 10 ? '0' + time % 60 : time % 60}`}</h1>
       <button onClick={() => {setRunning((cur) => !cur)}}>{running ? "stop" : "start"}</button>
     </div>
