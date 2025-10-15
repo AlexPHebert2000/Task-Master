98e880d52d3a8e1fa2585ead97fd72102143de9e
Alex <alexphebert@gmail.com>
2025-10-15 12:16:00 -0500

    ADD timer

--- src/views/Pomodoro.jsx
+++ src/views/Pomodoro.jsx
@@ -1,9 +1,29 @@
 import {useState, useEffect} from "react";
 
 export default () => {
+  
+  const [mode, setMode] = useState("focus");
+  const modes = {
+    "focus": 25 * 60,
+    "short": 5 * 60,
+    "long": 15 * 60
+  };
+
+  const [running, setRunning] = useState(true);
+
+  const [time, setTime] = useState(modes[mode]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((time) => time - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  },[]);
+
   return (
     <>
-    Pomodoro
+      <h1>{`${Math.floor(time / 60)}:${time % 60 < 10 ? '0' + time % 60 : time % 60}`}</h1>
     </>
   )
 }
