c0f4ca77bd91a40affd2b13ba6d0445068d88cd2
Alex <alexphebert@gmail.com>
2025-10-18 11:19:02 -0500

    ADD display leaderboard

--- src/views/Leaderboard.jsx
+++ src/views/Leaderboard.jsx
@@ -12,11 +12,20 @@ export default () => {
     }
     leaderboardFetch();
   }, [])
-
   return (
     <div>
-      LeaderBoard
-      <div>{JSON.stringify(leaderboard)}</div>
+      <h1 className="font-bold text-3xl">Leaderboard</h1>
+      <div className="justify-center">
+        {
+          leaderboard ?
+          <ol>
+            {
+            leaderboard.leaderBoard.map(({user, percentage}) => <li key={user + percentage}>{`${user} : ${percentage}`}</li>)
+            }
+          </ol>
+          : <h2>Please Wait</h2>
+        }
+      </div>
     </div>
   )
 }
