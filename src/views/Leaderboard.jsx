2674260f74d04ae661f957c3fffbadb4dc8013ac
Alex <alexphebert@gmail.com>
2025-10-20 22:21:19 -0500

    ADD leaderboard styling

--- src/views/Leaderboard.jsx
+++ src/views/Leaderboard.jsx
@@ -15,15 +15,28 @@ export default () => {
 
   return (
     <div>
-      <h1 className="font-bold text-3xl">Leaderboard</h1>
-      <div className="justify-center">
+      <h1 className="font-bold text-3xl mb-5">Leaderboard</h1>
+      <div className="items-center">
         {
           leaderboard ?
-          <ol>
+          <>
+          <div className="px-2 py-1 bg-amber-200 border-2 ">
+            <p className="font-semibold">{`🥇 ${leaderboard.leaderBoard[0].user} : ${Number(leaderboard.leaderBoard[0].percentage).toFixed(2)}`}</p>
+          </div>
+          <div className="px-2 py-1 bg-gray-300 border-2 my-2">
+            <p className="font-semibold">{`🥈 ${leaderboard.leaderBoard[1].user} : ${Number(leaderboard.leaderBoard[1].percentage).toFixed(2)}`}</p>
+          </div>
+          <div className="px-2 py-1 bg-orange-300 border-2 my-2">
+            <p className="font-semibold">{`🥉 ${leaderboard.leaderBoard[2].user} : ${Number(leaderboard.leaderBoard[0].percentage).toFixed(2)}`}</p>
+          </div>
             {
-            leaderboard.leaderBoard.map(({user, percentage}, index) => <li key={index}>{`${user} : ${Number(percentage).toFixed(2)}`}</li>)
+              leaderboard.leaderBoard.slice(3).map(({user, percentage}, index) => (
+                <div className="px-2 py-1 border-2 my-2">
+                  <p key={index}>{`${user} : ${Number(percentage).toFixed(2)}`}</p>
+                </div>
+            ))
             }
-          </ol>
+          </>
           : <h2>Please Wait</h2>
         }
       </div>
